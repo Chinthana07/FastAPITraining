85944ba2dcb829684e78749d7ebccf5cd7bde668
Chinthana <2025cs_chinthanaks_b@nie.ac.in>
2026-09-22 16:02:08 +0530

    Pymongo

---
+++ Day/Day2/FastAPIMongo/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from pymongo import AsyncMongoClient
+app = FastAPI()
+client = AsyncMongoClient("mongodb://localhost:27012/")
+db=client["college"]
+
+#select collection
+students_collection = db["student"]
+@app.get("/")
+async def home():
+    return {
+        "message":"FastAPI with MongoDB is running"
+    }
+
+@app.get("/health")
+async def health():
+    result = await db.command("ping")
+    return {"mongodb":"Connected", "ping":result["ok"]}
+
+    
